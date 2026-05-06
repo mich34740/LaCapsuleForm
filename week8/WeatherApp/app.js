93b47a2259d3dec474fc8ca30a1aabc249982554
mich34740 <michgm34@gmail.com>
2026-05-06 21:22:40 +0200

    wheatherApp

---
+++ week8/WeatherApp/app.js
@@ -0,0 +1,27 @@
+
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const dotenv = require('dotenv');
+
+const envFile =
+  process.env.NODE_ENV === 'production'
+    ? '.env.production'
+    : '.env';
+dotenv.config({ path: envFile });
+
+var express = require('express');
+var app = express();
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
---
+++ week8/WeatherApp/routes/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/weather/:city', (req, res) => {  
+	fetch(`https://api.openweathermap.org/data/2.5/weather?q=${req.params.city}&appid=${process.env.OPENWEATHER_API_KEY}&units=metric`)
+				.then(response => response.json())
+				.then(Data => {
+					res.status(200).json({result: true, city: Data,});
+  });
+});      
+
+router.get('/weather/forecast/:city', (req, res) => {  
+  if (!req.params.city) { return res.json({result: false, error: "aucune ville"})};
+  fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${req.params.city}&appid=${process.env.OPENWEATHER_API_KEY}&units=metric`)
+				.then(response => response.json())
+				.then(Data => {
+					res.status(200).json({result: true, city: Data,});
+  });
+});      
+
+module.exports = router;
