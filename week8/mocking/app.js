46be60214c1b977b4c300efb82bdbba7af5080df
mich34740 <michgm34@gmail.com>
2026-05-15 18:50:38 +0200

    test

--- week8/mocking/app.js
+++ week8/mocking/app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const weatherRouter = require("./routes/weather");
 
 var app = express();
 
@@ -14,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use("/weather", weatherRouter);
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
